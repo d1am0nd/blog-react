93552702c5514e2973cea5674db7d84e8df61a18
devk <devkordesg@gmail.com>
2017-12-14 23:56:25 +0100

    Analytics

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,13 @@ import App from './Client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {connect, Provider} from 'react-redux';
 import {clientStore} from './store';
+import client from '../../config/client.json';
+import ReactGA from 'react-ga';
 
+if (client.env === 'prod' || client.env === 'production') {
+  ReactGA.initialize(client.analytics);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 const rootDom = document.getElementById('root');
 
 ReactDOM.hydrate(
