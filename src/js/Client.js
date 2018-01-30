9dc28e36339ec1cbfaaf9d62c60d0afef5a3a02c
devk <devkordesg@gmail.com>
2018-01-30 21:44:00 +0100

    ReactGA now reacts to route changes

--- src/js/Client.js
+++ src/js/Client.js
@@ -2,8 +2,22 @@ import React from 'react';
 import Layout from './components/Layout';
 import {StyleRoot} from 'radium';
 import {Provider, connect} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {Router} from 'react-router-dom';
 import {setDataLoaded} from './store/actions/miscActions';
+import client from '../../config/client.json';
+import history from './history';
+
+if ((client.env === 'prod' || client.env === 'production') &&
+  navigator.userAgent.indexOf('Speed Insights') == -1) {
+  ReactGA.initialize(client.analytics);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
+  history.listen((location, action) => {
+    console.log('here we are', location, action);
+    ReactGA.set({page: location.pathname});
+    ReactGA.pageview(location.pathname);
+  });
+}
 
 class App extends React.Component {
   componentDidMount() {
@@ -14,7 +28,7 @@ class App extends React.Component {
     return (
       <Provider store={this.props.store}>
         <StyleRoot>
-          <Router><Layout/></Router>
+          <Router history={history}><Layout/></Router>
         </StyleRoot>
       </Provider>
     );
--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,10 @@ import App from './Client';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {connect, Provider} from 'react-redux';
 import {clientStore} from './store';
-import client from '../../config/client.json';
-import ReactGA from 'react-ga';
 import './service-worker.js';
 
-if ((client.env === 'prod' || client.env === 'production') &&
-  navigator.userAgent.indexOf('Speed Insights') == -1) {
-  ReactGA.initialize(client.analytics);
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
+console.log(Router);
+
 const rootDom = document.getElementById('root');
 
 ReactDOM.hydrate(
